55341a49ab62153635b6aa4f4306425606462f27
Nabil2015 <nabilelaslaoui@gmail.com>
2017-12-27 02:42:45 +0100

    horaires route

--- config/conf.php
+++ config/conf.php
@@ -28,6 +28,7 @@ Router::connect('listeCours','cours/index');
 Router::connect('ajouter','cours/ajouter');
 Router::connect('listeContrats','contrats/index');
 Router::connect('afficherContrat','contrats/index');
+Router::connect('horaires','horaires/index');
 Router::connect('cockpit','cockpit/posts/index');
 Router::connect('page/:slug-:id','pages/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
 Router::connect('blog/:slug-:id','posts/view/id:([0-9]+)/slug:([a-z0-9\-]+)');
